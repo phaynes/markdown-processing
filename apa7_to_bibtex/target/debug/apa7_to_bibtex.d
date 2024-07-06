26e3c1f54b5a0c33e4b1bfcaf322826f65a2c498
Philip Haynes <Philip.haynes@me.com>
2024-07-07 08:34:47 +1000

    remove build

--- apa7_to_bibtex/target/debug/apa7_to_bibtex.d
+++
@@ -1,1 +0,0 @@
-/Users/philiphaynes/devel/markdown/apa7-to-bib-claude/apa7_to_bibtex/target/debug/apa7_to_bibtex: /Users/philiphaynes/devel/markdown/apa7-to-bib-claude/apa7_to_bibtex/src/main.rs
--- apa7_to_bibtex/target/debug/deps/apa7_to_bibtex-ec25c202feb4f698.d
+++
@@ -1,5 +0,0 @@
-/Users/philiphaynes/devel/markdown/apa7-to-bib-claude/apa7_to_bibtex/target/debug/deps/apa7_to_bibtex-ec25c202feb4f698: src/main.rs
-
-/Users/philiphaynes/devel/markdown/apa7-to-bib-claude/apa7_to_bibtex/target/debug/deps/apa7_to_bibtex-ec25c202feb4f698.d: src/main.rs
-
-src/main.rs:
